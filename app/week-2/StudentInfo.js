5c166438a09e6819b100d7ccc82959b7b0f9a721
Vishavjeet Singh <vishavjeetgill133@gmail.com>
2026-01-22 16:26:08 -0700

    Completed Week 2 Assignment

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Vishavjeet Singh</p>
+      <p>Student ID: 000966816</p>
+      <Link href="https://github.com/Vishavjeet-Singh-44/cprg306-assignments">
+        My GitHub Repository Link
+      </Link>
+    </div>
+  );
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./StudentInfo";
+
+export default function Page() {
+  return (
+    <main>
+      <h1></h1>
+      <StudentInfo />
+    </main>
+  );
+}
